64c1f9ff6252526678e8780d3b9415e60801c03a
VPinezi <50252684+vpinezi@users.noreply.github.com>
2022-11-08 08:56:38 -0300

    feature
    Implementação do banner e sessão de favoritos

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,21 @@ import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/TimeLine";
+import { StyledFavorites } from "../src/components/Favorites";
 
 function HomePage() {
-
-    // const estilosDaHomePage = { 
-    //     //backgroundColor: "red" 
-    // };
-
-    //console.log (config.playlists);
-
     return (
         <>
         <CSSReset />
         <div style={{
                 display: "flex",
                 flexDirection: "column",
-                flex: 1,
-                // backgroundColor: "red",
+                flex: 1
             }}>
             <Menu />
             <Header />
             <TimeLine playlists={config.playlists}/>
+            <Favorites Favorites={config.favorites} />
         </div>
         </>
         
@@ -33,14 +27,6 @@ function HomePage() {
   
 export default HomePage
 
-// function Menu(){
-//     return(
-//         <div>
-//             Menu
-//         </div>
-//     )
-// }
-
 const StyledHeader = styled.div`
     img {
         width: 80px;
@@ -48,18 +34,41 @@ const StyledHeader = styled.div`
         border-radius: 50%;
     }
     .user-info {
-        margin-top: 50px;
+        margin-top: 10px;
         display: flex;
         align-items: center;
         width: 100%;
         padding: 16px 32px;
         gap: 16px;
     }
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: #f5f7f8;
+        width: 1512px;
+        height: 230px;
+        object-fit: cover;
+        
+    }
+    .banner {
+        left: 0px;
+        top: 56px;
+        border-radius: 0%;
+        display: block; 
+        width: 1512px;
+        height: 230px;
+        object-fit: cover;
+        object-position: 50% 50%;
+    }
 `;
+
 function Header(){
     return (
         <StyledHeader>
-            {/*<img src="banner" />*/}
+            <div className="container">
+                <img className="banner" src={config.banner} />
+            </div> 
             <section className="user-info">
                 <img src={`https://github.com/${config.github}.png`} />
                 <div>
@@ -102,19 +111,35 @@ function TimeLine(props){
                         </div>
                     </section>
                 )
-
-                // return videos.map((video) => {
-                //     return(
-                //         <a href={video.url}>
-                //             <img src={video.thumb}/>
-                //             <span>
-                //                 {video.title}
-                //             </span>
-                //         </a>
-                //     )
-                // })
             })}
         </StyledTimeline>
     )
 }
 
+function Favorites(props){
+    //console.log("Dentro do componente", props.Favorites);
+    const favorites = props.Favorites;
+
+    return (
+        <StyledFavorites>
+            <h2>AluraTube Favoritos</h2>
+            <section>
+                <div>
+                    {
+                        favorites.map((favorite) => {
+                            return(
+                                <a href={favorite.url}>
+                                    <img src={favorite.thumb} />
+                                    <span>
+                                        {favorite.name}
+                                    </span>
+                                </a>  
+                            )
+                        })
+                    }
+                </div>
+                
+            </section>
+        </StyledFavorites>      
+    )
+}
