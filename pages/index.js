18d156de54b3aaf5767ed4067473c09539646452
VPinezi <50252684+vpinezi@users.noreply.github.com>
2022-11-09 20:12:42 -0300

    Aula 3 - final

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/TimeLine";
 import { StyledFavorites } from "../src/components/Favorites";
 
 function HomePage() {
-    const [valorDoFiltro, setValorDoFiltro] = React.useState("Angular");
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
     return (
         <>
-        <CSSReset />
         <div style={{
                 display: "flex",
                 flexDirection: "column",
@@ -31,6 +29,8 @@ function HomePage() {
 export default HomePage
 
 const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+    
     img {
         width: 80px;
         height: 80px;
